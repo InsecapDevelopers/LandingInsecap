2c1711e85ab2e4863bdc0175f94066606f4ef0bb
ernitoft <ernes.fuenzalida@alumnos.ucn.cl>
2026-03-05 09:59:00 -0300

    feat: update button and icon colors in Modal and NumberTicker components

--- src/components/ui/animated-modal.tsx
+++ src/components/ui/animated-modal.tsx
@@ -52,7 +52,7 @@ export const ModalTrigger = ({
   return (
     <button
       className={cn(
-        "px-4 py-2 rounded-md text-black dark:text-white text-center relative overflow-hidden",
+        "px-4 py-2 rounded-md text-blue-950 dark:text-white text-center relative overflow-hidden",
         className
       )}
       onClick={() => {
@@ -215,7 +215,7 @@ const CloseIcon = () => {
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="text-black dark:text-white h-4 w-4 group-hover:scale-125 group-hover:rotate-3 transition duration-200"
+        className="text-blue-950 dark:text-white h-4 w-4 group-hover:scale-125 group-hover:rotate-3 transition duration-200"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M18 6l-12 12" />
