8e66a6e5da0a12d8b9cf3a1b88bca076fbe7df67
ernitoft <ernes.fuenzalida@alumnos.ucn.cl>
2026-03-06 15:55:11 -0300

    feat: integrate and initialize Microsoft Clarity for analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import Clarity from "@microsoft/clarity";
 import App from "./App.tsx";
 import "./index.css";
+
+Clarity.init("vr6vp2at33");
 
 createRoot(document.getElementById("root")!).render(<App />);
